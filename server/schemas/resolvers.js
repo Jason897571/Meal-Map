2b4aa2e9bf99597af11372c8545a210ced5a5efd
garrethil <garrethildebrandt@gmail.com>
2024-04-30 19:55:30 -0400

    add resolvers and typeDefs to schemas directory

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,91 @@
+const { User, reviewSchema } = require('../models')
+const { signToken, AuthenticationError } = require('../utils/Auth');
+
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+
+            throw AuthenticationError;
+        },
+
+        user: async (parent, { userId }) => {
+            return User.findOne({ _id: userId })
+        },
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password}) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user }
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if (!correctPassword) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return { token, profile };
+        },
+        addFavourite: async (parent, { locationId }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { favourites: locationId }},
+                    {
+                        new: true,
+                      }
+                );
+            }
+            throw AuthenticationError;
+        },
+        removeFavourite: async (parent, { locationId }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { favourites: locationId }},
+                    { new: true }
+                )
+            }
+            throw AuthenticationError;
+        },
+        addReview: async (parent, { locationId, rating, review }, context) => {
+            if (context.user) {
+                const newReview = new reviewSchema({ locationId, rating, review });
+
+                await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { reviews: newReview }},
+                    { new: true }
+                )
+            }
+            throw AuthenticationError;
+        },
+        removeReview: async (parent, { reviewId }, context ) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { reviews: { _id: reviewId }}},
+                    { new: true }
+                )
+            }
+
+            }
+        }
+
+    }
+
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String
+        favourites: [ID]
+        reviews: [reviewInput]
+    }
+    type Review {
+        _id: ID
+        username: String
+        locationID: String
+        rating: Int!
+        review: String
+    }
+    input ReviewInput {
+        _id: ID
+        username: String
+        locationID: String
+        rating: Int!
+        review: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+      }
+    type Query {
+        me: User
+        user(userId: ID!): User
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        addReview(review: ReviewInput!): Review
+        addFavourite(userId: ID!, locationId: ID!): User
+        removeReview(reviewId: ID!): User
+        removeFavourite(userId: ID!, locationId: ID!): User
+    }
+`
