f183d3c7a55047685e42151d1df660c13b79672b
Jason897571 <king.gs1314@outlook.com>
2024-04-30 20:02:24 -0400

    complete top list restaurant component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import SearchBar from "../components/SearchBar"
+import TopResults from "../components/TopResults"
 
 export default function Home() {
     return(
         <>
             <SearchBar />
+            <TopResults />
         </>
     )
 }
