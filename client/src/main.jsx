39b8442a0920b9de6b9908af1938a2cdf000b71a
Jason897571 <king.gs1314@outlook.com>
2024-04-30 21:23:40 -0400

    complete sign up and sign in

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,6 @@ import './output.css'
 
 import App from './App.jsx';
 import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
 import ErrorPage from './pages/ErrorPage';
 import ResultPage from './pages/ResultPage';
 
@@ -18,12 +16,6 @@ const router = createBrowserRouter([
         {
           index: true,
           element: <Home />
-        }, {
-          path: '/login',
-          element: <Login />
-        }, {
-          path: '/signup',
-          element: <Signup />
         },
         {
             path: '/result',
