242caa0acfbaec904986ab4e3bd8b9ef87a52123
Jason897571 <king.gs1314@outlook.com>
2024-04-30 23:01:19 -0400

    complete structure for search page

--- client/src/pages/ResultPage.jsx
+++ client/src/pages/ResultPage.jsx
@@ -1,6 +1,12 @@
+import ListResult from "../components/ListResult";
+import MapResult from "../components/MapResult";
+
+
 export default function ResultPage() {
     return(
-        <>
-        </>
+        <div className="result-container">
+            <ListResult />
+            <MapResult />
+        </div>
     )
 }
