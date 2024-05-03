ac6c04fa4298df470f0edafd7a02a8935803d359
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-03 12:17:22 -0400

    I just changed the component name for better naming conventions

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,30 +1,31 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './output.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import App from './App.jsx';
-import SearchedPlaces from './pages/SearchedPlaces';
-import ErrorPage from './pages/ErrorPage';
-import ResultPage from './pages/ResultPage';
+import App from './App.jsx'
+import SearchedPlacesPage from './pages/SearchedPlacesPage'
+import ErrorPage from './pages/ErrorPage'
+import ResultPage from './pages/ResultPage'
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          index: true,
-          element: <SearchedPlaces />
-        },
-        {
-            path: '/result',
-            element: <ResultPage />
-          }
-      ]
-    },
-]);
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <SearchedPlacesPage />,
+      },
+      {
+        path: '/result',
+        element: <ResultPage />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<RouterProvider router={router} />
+  <RouterProvider router={router} />,
 )
--- client/src/pages/SearchedPlacesPage.jsx
+++ client/src/pages/SearchedPlacesPage.jsx
@@ -1,22 +1,33 @@
 import { useEffect, useState } from 'react'
 import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap'
 
-const SearchedPlaces = () => {
+const SearchedPlacesPage = () => {
   // create state which will hold/save the users searched place into array
   const [searchedPlaces, setSearchedPlaces] = useState([])
 
   // create state which will hold our search input value
   const [searchInput, setSearchInput] = useState('')
 
+  const noResults = (
+    <>
+      {' '}
+      <h3 className="noResult text-3lg capitalize hover:uppercase bg-slate-50">
+        Please search for place to begin!
+      </h3>
+    </>
+  )
+
   return (
     <>
       <div className="text-light bg-pink-500 p-5">
         <Container>
-          <h1 className="fs-1 mb-5 text-left">Search for Your Cravings!</h1>
+          <h1 className="fs-1 mb-4 uppercase  text-left font-bold">
+            Search for Your Cravings!
+          </h1>
           <Form>
             <Row>
               <Col xs={12} md={8}>
-                <Form.Control placeholder="Search for anything..." />
+                <Form.Control id="#link" placeholder="Search for anything..." />
               </Col>
               <Col xs={12} md={4}>
                 {' '}
@@ -25,9 +36,17 @@ const SearchedPlaces = () => {
             </Row>
           </Form>{' '}
         </Container>
+
+        <Container>
+          <h2 className="pt-5">
+            {searchedPlaces.length
+              ? `Viewing ${searchedPlaces.length}`
+              : noResults}
+          </h2>
+        </Container>
       </div>
     </>
   )
 }
 
-export default SearchedPlaces
+export default SearchedPlacesPage
