e5326e75b3ea80db7bac1ac4952a06445e3213a6
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-03 21:37:11 -0400

    created the our mission component then integrated it in about us page

---
+++ client/src/components/AboutUsComponents/OurMission.jsx
@@ -0,0 +1,14 @@
+// Summary: This component displays the mission of the website. It is a simple component that displays the mission statement of the website.
+export default function OurMission() {
+  return (
+    <div className="mt-5">
+      <h1 className="text-3xl  mb-4 uppercase font-bold">Our Mission</h1>
+      <p className="text-lg fw-medium">
+        Our mission is to connect food enthusiasts, explorers, and local experts
+        in one space. Whether you're a foodie looking for hidden gems in your
+        city or a traveler seeking authentic culinary experiences abroad, our
+        platform is designed to inspire and guide you.
+      </p>
+    </div>
+  )
+}
