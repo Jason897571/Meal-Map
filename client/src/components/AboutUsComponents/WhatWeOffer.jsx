e9e246ecf69211c57f106341d6189ab3ba340ec0
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-03 21:37:27 -0400

    created the what we offer component then integrated it in about us page

---
+++ client/src/components/AboutUsComponents/WhatWeOffer.jsx
@@ -0,0 +1,15 @@
+export default function WhatWeOffer() {
+  return (
+    <>
+      <div className="mt-5">
+        <h1 className="text-3xl  mb-4 uppercase font-bold">What We Offer</h1>
+        <p className="text-lg fw-medium">
+          Our app is a curated collection of recommendations from fellow users
+          and trusted sources. It's more than just a list of places; it's a
+          community-driven guidebook crafted by people who are as passionate
+          about good food and memorable adventures as you are.
+        </p>
+      </div>
+    </>
+  )
+}
