12a8329f819eeb8bf9ccf33d8831de9f27abe109
garrethil <garrethildebrandt@gmail.com>
2024-05-06 14:46:31 -0400

    add dependecies for Map display and basic layout

--- client/src/components/MapResult/index.jsx
+++ client/src/components/MapResult/index.jsx
@@ -1,9 +1,19 @@
-const MapResult = (props) => {
-  return (
-    <div className="map-list">
-      <h1 className="text-2xl">Map Result</h1>
-    </div>
-  )
-}
+import { useState } from 'react'
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from '@vis.gl/react-google-maps'
 
-export default MapResult
+const APIKEY = process.env.GOOGLE_API_KEY
+
+export default function MapResult(data) {
+  return
+  ;<APIProvider>
+    <div style={{ height: '100vh' }} className="map-list">
+      <Map zoom={9} center={restaurant.location}></Map>
+    </div>
+  </APIProvider>
+}
